058d1448e61b99bf9497ddd4860965c0065e428e
Anamika Yadav <yadav_ana@xoriant.com>
2026-06-30 13:30:50 +0530

    Add hello-worl program

---
+++ basic-hello-world/App.js
@@ -0,0 +1,53 @@
+
+/**
+ * <div id="root">
+ *      <h1 id="heading1">HELLO</h1>
+ * </div>
+ */
+// const heading = React.createElement("h1", { id: "heading1", xyz: "dfjhd" }, "HELLO");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+/**
+ * Creates a heading element with a specific ID and text content.
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1 id="heading1">HELLO</h1>
+ *          <h2 id="heading2">2 HELLO</h1>
+ *      </div>
+ * </div>
+ */
+// const contentchild = React.createElement(
+//     "div",
+//     { id: "child" },
+//     [React.createElement("h1", { id: "heading1" }, "HELLO"),
+//     React.createElement("h2", { id: "heading2" }, "2 HELLO")]);
+// const contentparent = React.createElement("div", { id: "parent" }, contentchild);
+// console.log(contentparent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(contentparent);
+
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1 id="heading1">HELLO</h1>
+ *         <h2 id="heading2">2 HELLO</h1>
+ *      </div>
+ *      <div id="sibling">
+ *          <h1 id="heading3">3 HELLO</h1>
+ *          <h2 id="heading4">4 HELLO</h1>
+ *      </div>
+ * </div>
+ */
+
+const content = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" },
+        [React.createElement("h1", { id: "heading1" }, "HELLO"),
+        React.createElement("h2", { id: "heading2" }, "2 HELLO")]),
+    React.createElement("div", { id: "sibling" },
+        [React.createElement("h1", { id: "heading3" }, "3 HELLO"),
+        React.createElement("h2", { id: "heading4" }, "4 HELLO")])]);
+
+        console.log(content);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(content);
