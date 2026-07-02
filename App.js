e6a8db2901f408e0916614e2176c8b6adfcbe09d
Anamika Yadav <yadav_ana@xoriant.com>
2026-07-02 16:01:47 +0530

    igniting our app

--- App.js
+++ App.js
@@ -27,6 +27,10 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(contentparent);
 
+// const content = React.createElement("div", { title: "parent-div" },
+//     [React.createElement("span", { id: "spanid" }, "my 1st span"),
+//     React.createElement("h2", { title: "h2-title" }, "my H2")])
+
 /**
  * <div id="parent">
  *      <div id="child">
@@ -40,6 +44,8 @@
  * </div>
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 const content = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" },
         [React.createElement("h1", { id: "heading1" }, "HELLO"),
@@ -48,6 +54,6 @@ const content = React.createElement("div", { id: "parent" },
         [React.createElement("h1", { id: "heading3" }, "3 HELLO"),
         React.createElement("h2", { id: "heading4" }, "4 HELLO")])]);
 
-        console.log(content);
+console.log(content);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(content);
