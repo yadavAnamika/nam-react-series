774dac31ea29bc7ef044f4ef10e62e2a8a5c84b0
Anamika Yadav <yadav_ana@xoriant.com>
2026-07-03 12:57:25 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,59 +1,29 @@
+import ReactDOM from "react-dom/client"
 
-/**
- * <div id="root">
- *      <h1 id="heading1">HELLO</h1>
- * </div>
- */
-// const heading = React.createElement("h1", { id: "heading1", xyz: "dfjhd" }, "HELLO");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+// React Element
 
-/**
- * Creates a heading element with a specific ID and text content.
- * <div id="parent">
- *      <div id="child">
- *          <h1 id="heading1">HELLO</h1>
- *          <h2 id="heading2">2 HELLO</h1>
- *      </div>
- * </div>
- */
-// const contentchild = React.createElement(
-//     "div",
-//     { id: "child" },
-//     [React.createElement("h1", { id: "heading1" }, "HELLO"),
-//     React.createElement("h2", { id: "heading2" }, "2 HELLO")]);
-// const contentparent = React.createElement("div", { id: "parent" }, contentchild);
-// console.log(contentparent);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(contentparent);
+const head = <h1 className="head1" tabIndex="1">HELLO</h1>;
 
-// const content = React.createElement("div", { title: "parent-div" },
-//     [React.createElement("span", { id: "spanid" }, "my 1st span"),
-//     React.createElement("h2", { title: "h2-title" }, "my H2")])
+//React Component
+const TitleComponent = () => (
+    <h2 className="titleCls">Hello title from ANOTHER FUNCTIONAL component</h2>
+)
 
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1 id="heading1">HELLO</h1>
- *         <h2 id="heading2">2 HELLO</h1>
- *      </div>
- *      <div id="sibling">
- *          <h1 id="heading3">3 HELLO</h1>
- *          <h2 id="heading4">4 HELLO</h1>
- *      </div>
- * </div>
- */
+//React Element
+const title = (
+    <h1 className="titleEle">Hello React Element JSX</h1>
+);
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-const content = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child" },
-        [React.createElement("h1", { id: "heading1" }, "HELLO"),
-        React.createElement("h2", { id: "heading2" }, "2 HELLO")]),
-    React.createElement("div", { id: "sibling" },
-        [React.createElement("h1", { id: "heading3" }, "3 HELLO"),
-        React.createElement("h2", { id: "heading4" }, "4 HELLO")])]);
+// React function based component
+const HeadingComponent = () => {
+    return (
+        <div id="parent">
+            <TitleComponent />
+            {TitleComponent()}
+            {title}
+            <h1 className="head1" tabIndex="1">Hello functional component h1</h1>
+        </div>)
+};
 
-console.log(content);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(content);
+root.render(<HeadingComponent />);
